d41371eda9580c0f1f7e68c67374abfc74f2a069
Krog-13 <virtualspook@gmail.com>
2024-03-31 17:58:14 +0500

    test

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,19 @@
 let tg = window.Telegram.WebApp;
 let buy = document.getElementById("buy")
 let order = document.getElementById("order")
-
+tg.expand()
 
 buy.addEventListener("click", () => {
     document.getElementById("main").style.display = "none"
     document.getElementById("form").style.display = "block"
-    document.getElementById("address").value = tg.initDataUnsafe.first_name
+    document.getElementById("address").value = "Petrick"
 });
 
 order.addEventListener("click", () => {
+    let address = document.getElementById("address")
+    let data = {
+        address: address
+    }
+    tg.sendData(JSON.stringify(data))
     tg.close()
 })
